29e53e65d8db3bfdf12423c1d199b9643d3daecb
Isabella Basso <isabellabdoamaral@gmail.com>
2026-05-27 21:58:07 -0300

    sokol-tools: patch tint integer range signature before fibs build

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+TINT_RANGE_H="ext/tint-extract/src/tint/lang/core/ir/analysis/integer_range_analysis.h"
+TINT_RANGE_CC="ext/tint-extract/src/tint/lang/core/ir/analysis/integer_range_analysis.cc"
+
+if [ -f "$TINT_RANGE_H" ] && [ -f "$TINT_RANGE_CC" ]; then
+    perl -pi -e 's/GetInfo\(const FunctionParam\* param, uint32_t index = 0\)/GetInfo(const FunctionParam* param, int index = 0)/g' "$TINT_RANGE_H"
+    perl -pi -e 's/GetInfo\(const FunctionParam\* param, uint32_t index\)/GetInfo(const FunctionParam* param, int index)/g' "$TINT_RANGE_CC"
+fi
+
 ./fibs build
 if [ $(uname -s) = 'Darwin' ]; then
     cp .fibs/build/macos-make-release/sokol-shdc
